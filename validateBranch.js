9224b20eb7d8bb75777e80580bbef1a6b9d815cd
Chris Gaona <chris@specialtacticssolutions.com>
2019-07-12 09:55:35 -0700

    Another small change

--- validateBranch.js
+++ validateBranch.js
@@ -3,6 +3,7 @@ const { stdout, stderr, code } = shell.exec(
   'git branch | grep * | cut -d \' \' -f2',
   { silent: true },
 )
+console.log('stdout', stdout)
 if (stdout === 'master') {
   throw new Error('You cannot push directly to the master branch.')
 }
